d6bf7545911a77ce5dc8e535bdce291d0528a100
Paul McNab <mac.paul.1965@gmail.com>
2014-01-31 11:15:24 +1000

    Adding initial code
    Adding the initial config, etc

---
+++ source/core/prApplication.h
@@ -0,0 +1,143 @@
+/**
+ * Copyright 2014 Paul Michael McNab
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+
+#ifndef __PRAPPLICATION_H
+#define __PRAPPLICATION_H
+
+
+#include "prTypes.h"
+
+
+// Forward declarations
+class prWindow;
+
+
+/** 
+ * Interface class for all the application type classes.
+ *
+ * The class for a specification application type should implement,
+ *
+ * DisplayCreate and Run. The game class should implement the rest
+ *
+ * @see    DisplayCreate
+ * @see    Run
+ */
+class prApplication
+{
+public:
+    
+    /**
+     * Ctor
+     */
+    prApplication();
+
+    /**
+     * Dtor
+     */
+    virtual ~prApplication();
+
+    /** 
+     * Creates the application display.
+     *
+     * @param      width       - Display width.
+     * @param      height      - Display height.
+     * @param      pWindowName - The name of the window, which will only appear in windowed apps
+     *
+     * @note       This call will destroy the previous display if it exists.
+     *
+     * @return     PRTRUE if the display was created successfully.
+     * @return     PRFALSE if the display was NOT created successfully.
+     */
+    virtual PRBOOL DisplayCreate(u32 width, u32 height, const char *pWindowName) = 0;
+
+    /** 
+     * Updates the application.
+     *
+     * @param      dt - Delta time
+     */
+    virtual void Update(f32 dt) = 0;
+
+    /**
+     * Draws the application.
+     */
+    virtual void Draw() = 0;
+
+    /**
+     * Runs the game.
+     *
+     * @note       Depending on the platform this function may the run the game continuously, and
+     * @note       may not exit until the game is complete, or
+     * @note       on other platforms it will be called once per frame.
+     *
+     * @return     PRTRUE if the game is running.
+     * @return     PRFALSE if the game is NOT running.
+     */
+    virtual PRBOOL Run() = 0;
+
+    /**
+     * Notification callback that is received when the application is
+     *
+     * being activated.
+     */
+    virtual void OnActivate() = 0;
+
+    /**
+     * Notification callback that is received when the application is
+     *
+     * being deactivated.
+     */
+    virtual void OnDeactivate() = 0;
+
+    /**
+     * Notification callback that is received when the application is
+     *
+     * exitting.
+     */
+    virtual void OnExit() = 0;
+
+    /**
+     * Notification callback that is received when the application is
+     *
+     * low on memory. Allows the game to free memory if possible
+     */
+    virtual void OnLowMemory() = 0;
+
+    /**
+     * Gets a pointer to the applications window.
+     *
+     * @return     Pointer to the applications window.
+     * @return     May return NULL.
+     */
+    prWindow *GetAppWindow() const { return m_pWindow; }
+
+    /**
+     * Gets the applications running status.
+     *
+     * @return     PRTRUE if the game is running.
+     * @return     PRFALSE if the game is NOT running.
+     */
+    PRBOOL GetIsRunning() const { return m_running; }
+
+
+protected:
+    
+    prWindow   *m_pWindow;
+    PRBOOL      m_running;
+};
+
+
+#endif//__PRAPPLICATION_H
---
+++ source/prConfig.h
@@ -0,0 +1,160 @@
+/**
+ *  Copyright 2014 Paul Michael McNab
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+
+#ifndef __PRCONFIG_H
+#define __PRCONFIG_H
+
+
+// Audio platform
+#define AUDIO_PC        1
+#define AUDIO_IPHONE    2
+#define AUDIO_BADA      3
+#define AUDIO_ANDROID   4
+#define AUDIO_MAC       5
+#define AUDIO_LINUX     6
+
+
+// Determine platform. First check for PC
+#if defined(_MSC_VER)
+    #define PLATFORM_PC
+    #define AUDIO_TYPE  AUDIO_PC
+
+// Else, Is it the iphone?
+#elif defined(__APPLE_CC__)
+    #define PLATFORM_IOS
+    #define AUDIO_TYPE  AUDIO_IPHONE
+
+// Or, Is it the bada? Android?
+#elif defined(__GNUC__)
+    // Bada?
+    #if defined(SHP)
+        #define PLATFORM_BADA
+        #define AUDIO_TYPE  AUDIO_BADA
+    
+    // Linux
+    #elif defined(linux)
+        #define PLATFORM_LINUX
+        #define AUDIO_TYPE  AUDIO_LINUX
+
+    // Android
+    #else
+        #define PLATFORM_ANDROID
+        #define AUDIO_TYPE  AUDIO_ANDROID
+    #endif
+
+#else
+    #error No platform has been defined!
+
+#endif
+
+
+// ----------------------------------------------------------------------------
+// Use these defines to configure the engine setup
+// ----------------------------------------------------------------------------
+#ifndef SOUND_ALLOW                                     // Use the sound system. Can be removed for debugging.
+//#define SOUND_ALLOW
+#endif
+
+#ifndef REMOVE_EXCEPTIONS                               // Allows try/catch code to be removed. For example bada doesn't allow try/catch code.
+#define REMOVE_EXCEPTIONS
+#endif
+
+#ifndef ALLOW_PROTEUS_NAMESPACE                         // Allows namespace to be removed.
+//#define ALLOW_PROTEUS_NAMESPACE
+#endif
+
+#ifndef SHOW_MESSAGES                                   // Allows the TODO messages to be displayed by the compiler.
+#define SHOW_MESSAGES
+#endif
+
+#ifndef PROTEUS_ALLOW_CONSOLE                           // Allows the debug console to be optionally removed. (PC only)
+#define PROTEUS_ALLOW_CONSOLE
+#endif
+
+#ifndef PROTEUS_ALLOW_WINDOW_MINIMIZE                   // Allows the PC window to have a minimize button
+//#define PROTEUS_ALLOW_WINDOW_MINIMIZE
+#endif
+
+#ifndef PROTEUS_ALLOW_WINDOW_MAXIMIZE                   // Allows the PC window to have a maximize button
+//#define PROTEUS_ALLOW_WINDOW_MAXIMIZE
+#endif
+
+#ifndef PROTEUS_ALLOW_RESIZEABLE_WINDOW                 // Allows the PC window to be resizeable.
+//#define PROTEUS_ALLOW_RESIZEABLE_WINDOW
+#endif
+
+
+// ----------------------------------------------------------------------------
+// Android config
+// ----------------------------------------------------------------------------
+#if defined(PLATFORM_ANDROID)
+#ifndef USE_OPENAL                                      // Allows the android to use OpenAL
+//#define USE_OPENAL
+#endif
+#endif
+
+
+// ----------------------------------------------------------------------------
+// Bada config
+// ----------------------------------------------------------------------------
+#if defined(PLATFORM_BADA)                              // Bada can't allow try/catch as they're not supported.
+#ifndef REMOVE_EXCEPTIONS                               // This also affects 'new' as 'new' no longer throws exceptions
+#define REMOVE_EXCEPTIONS
+#endif
+
+#ifndef ALLOW_PROTEUS_NAMESPACE                         // Prevents naming collisions.
+#define ALLOW_PROTEUS_NAMESPACE
+#endif
+
+#ifndef BADA_SDK_2                                      // Bada SDK version 2 and above have different C library implementations
+#define BADA_SDK_2                                      // Need to be careful when using standard C code.
+#endif
+
+#endif//PLATFORM_BADA
+
+
+// ----------------------------------------------------------------------------
+// Namespace
+// ----------------------------------------------------------------------------
+#if defined(ALLOW_PROTEUS_NAMESPACE)                    // This code allows optional namespace usage.
+  #define PRNS_BEGIN        namespace Proteus {
+  #define PRNS              Proteus::
+  #define PRNS_END          }
+
+#else
+  #define PRNS_BEGIN
+  #define PRNS
+  #define PRNS_END
+
+#endif//ALLOW_PROTEUS_NAMESPACE
+
+
+// ----------------------------------------------------------------------------
+// Tweakbar
+// ----------------------------------------------------------------------------
+#ifndef PROTEUS_USE_ANT_TWEAK_BAR                       // Use AntTweakBar?
+#define PROTEUS_USE_ANT_TWEAK_BAR
+#endif
+
+
+// ----------------------------------------------------------------------------
+// Misc debug settings
+// ----------------------------------------------------------------------------
+//#define DEBUG_OPENGL
+
+
+#endif//__PRCONFIG_H
